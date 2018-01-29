e363972583b88fe49a9b93741064ea6fd0bf1d73
karbergja <karbergja@et.bsz.dresden.de>
2018-01-29 14:07:35 +0100

    add git config sh

---
+++ setProxy.sh
@@ -0,0 +1,9 @@
+git config --global http.proxy http://kjs-01.sv.dresden.de:3128
+
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
