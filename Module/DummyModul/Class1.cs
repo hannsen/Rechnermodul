e5e293bb048d24670c7fd46d1fa2d8d58a9664a4
lehmannka <lehmannka@et.bsz.dresden.de>
2018-02-06 10:46:49 +0100

    Whatever

---
+++ Module/DummyModul/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RechnermodulBibliothek;
+
+namespace DummyModul
+{
+    public class Class1 : RechnermodulBibliothek.AbstractModule
+    {
+        public Class1() {
+            this.setName("MeinDummyModul");
+            this.setDescription("asdfgh");
+        }
+    }
+}
--- Rechnermodul/Rechnermodul/rechnermodul.cs
+++ Rechnermodul/Rechnermodul/rechnermodul.cs
@@ -8,11 +8,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Reflection;
+using System.IO;
+using RechnermodulBibliothek;
+
 namespace Rechnermodul
 {
     public partial class rechnermodul : Form
     {
-        private Type[] modules; 
+        private List<RechnermodulBibliothek.ModulInterface> modules = new List<RechnermodulBibliothek.ModulInterface>(); 
 
         public rechnermodul()
         {
@@ -22,11 +26,33 @@ namespace Rechnermodul
         private void rechnermodul_Load(object sender, EventArgs e)
         {
             string[] modulesToLoad = new string[] {
-                ""
+                @"D:\RechnerModul\Module\DummyModul\obj\Debug\DummyModul.dll"
             };
 
             foreach (string dllPath in modulesToLoad) {
-                ;
+                var DLL = Assembly.LoadFile(@"D:\RechnerModul\Module\DummyModul\obj\Debug\DummyModul.dll");
+                RechnermodulBibliothek.ModulInterface modul = null;
+
+                foreach (Type type in DLL.GetExportedTypes()) {
+
+                    modul = Activator.CreateInstance(type) as RechnermodulBibliothek.ModulInterface;
+
+                    if (modul != null) {
+                        continue;
+                    }
+                }
+
+                if (modul == null)
+                {
+                    // COuld not load
+                }
+                else
+                {
+                    Console.Write(modul.getFriendlyName());
+                    this.modules.Add(modul);
+                }
+
+
             }
         }
     }
