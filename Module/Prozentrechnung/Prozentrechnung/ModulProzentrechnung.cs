7a974652e313711c184e7f26cd0a9f9c27887b6e
Felix Emmrich <emmricfe@et.bsz.dresden.de>
2018-02-08 07:57:30 +0100

    Rename, Structure

---
+++ Module/Prozentrechnung/Prozentrechnung/ModulProzentrechnung.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RechnermodulBibliothek;
+
+namespace Prozentrechnung
+{
+    public class ModulProzentrechnung : AbstractModule
+    {
+
+        public ModulProzentrechnung()
+        {
+
+        }
+
+    }
+
+    private class ProzentDazuFnc : FunctionInterface
+    {
+
+    }
+
+    private class ProzentWegFnc : FunctionInterface
+    {
+
+    }
+
+    private class ProzentDavonFnc : FunctionInterface
+    {
+
+    }
+
+    private class ProzentSatzFnc : FunctionInterface
+    {
+
+    }
+
+    private class BruttoAusNettoFnc : FunctionInterface
+    {
+
+    }
+
+    private class NettoAusBruttoFnc : FunctionInterface
+    {
+
+    }
+}
